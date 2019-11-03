6daa0cd70bef36794487975ef70b6f81a669576b
Ehmad <justEhmadSaeed@gmail.com>
2019-11-03 17:23:54 +0500

    👌IMPROVE File Handling

--- Queue/node.h
+++ Queue/node.h
@@ -12,15 +12,14 @@ private:
 public:
 	node *next = 0;
 	node();									// Default Constructor
-	node(int v);							// One parameterized Constructor
-	node(int v, int p);						// Two Parameterized Constructor
-  	node(ofstream &oFile);                  // Constructor to write a node to a file
+	node(int);							// One parameterized Constructor
+	node(int , int);						// Two Parameterized Constructor
 	node(ifstream &inFile);                 // Constructor to create node from a file
 	void writeNodeToFile(ofstream &outData); // Writes the node to the given file
 	void readNodeFromFile(ifstream &inData); // Reads the node from the given file and sets data according to the input
 	int getValue()const;
 	int getPriority() const;
-	void setValue(int v);
-	void setPriority(int p);
+	void setValue(int);
+	void setPriority(int);
 	void print();							// To print the value of node
 };
